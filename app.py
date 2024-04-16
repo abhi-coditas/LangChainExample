60908791763aea8298169c210fdce6f65fc70777
Abhilash Patil <abhilash@Abhilashs-MacBook-Pro.local>
2024-04-16 17:31:46 +0530

    saving work

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # Define the language model instance and the output parser function
 llm = Ollama(model='llama2')
+
 output_parser = StrOutputParser()
+
+webLoader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
+docs = webLoader.load()
+
+embeddings = OllamaEmbeddings()
+
+textSplitter = RecursiveCharacterTextSplitter()
+documents = textSplitter.split_documents(docs)
+vector = FAISS.from_document(documents, embeddings)
 
 # Define the chat prompt template with a system-generated response and user input
 prompt = ChatPromptTemplate.from_messages([
@@ -15,6 +29,8 @@ prompt = ChatPromptTemplate.from_messages([
 chain = prompt | llm | output_parser
 
 question = "How can langsmith help me with testing?"
+
+print("Loading...")
 
 # Invoke the chain with a valid input string
 result = chain.invoke({"input": question})
